d6f6597bea4983b2ebb29b2159109fefd9b2a555
versusdec <versusdec@gmail.com>
2018-06-14 22:20:19 +0300

    i dont give a fuck what im doing here

--- config/base.js
+++ config/base.js
@@ -9,7 +9,7 @@ const baseConf = (_path) => {
     // and add path and name to entry array
     const VENDORS_NAME = 'vendors';
     const entry = {
-        index: ['babel-polyfill', './src/index/index.js']
+        index: ['./src/index/index.js']
     };
 
     const plugins = Object.keys(entry).reduce((acc, name) => {
